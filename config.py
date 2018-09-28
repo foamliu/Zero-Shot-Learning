cd69d4bc395ed52ee9e2faf7a874203053281d15
Yang Liu <foamliu@yeah.net>
2018-09-28 14:27:47 +0800

    init commit

---
+++ config.py
@@ -0,0 +1,3 @@
+
+test_a_folder = 'ai_challenger_zsl2018_test_b_20180423'
+test_b_folder = 'ai_challenger_zsl2018_test_b_20180423'
---
+++ extract.py
@@ -0,0 +1,22 @@
+import os
+import zipfile
+
+from config import *
+from utils import ensure_folder
+
+
+def extract(folder):
+    filename = '{}.zip'.format(folder)
+    print('Extracting {}...'.format(filename))
+    with zipfile.ZipFile(filename, 'r') as zip_ref:
+        zip_ref.extractall('data')
+
+
+if __name__ == '__main__':
+    ensure_folder('data')
+
+    if not os.path.isdir(test_a_folder):
+        extract(test_a_folder)
+
+    if not os.path.isdir(test_b_folder):
+        extract(test_b_folder)
