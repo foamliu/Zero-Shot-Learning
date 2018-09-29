7948e6ce0d45862155d99e4976028196cd1a1c6f
Yang Liu <foamliu@yeah.net>
2018-09-29 14:05:19 +0800

    update

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 
-test_a_folder = 'ai_challenger_zsl2018_train_test_a_20180321'
-test_b_folder = 'ai_challenger_zsl2018_test_b_20180423'
+test_a_folder = 'data/ai_challenger_zsl2018_train_test_a_20180321'
+test_b_folder = 'data/ai_challenger_zsl2018_test_b_20180423'
