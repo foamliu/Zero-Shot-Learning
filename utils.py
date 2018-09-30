2334c7e8696595dd00f01e7a2df1ac6e32b08840
Yang Liu <foamliu@yeah.net>
2018-09-30 16:30:56 +0800

    update

--- utils.py
+++ utils.py
@@ -86,7 +86,7 @@ def accuracy(scores, targets):
 def save_checkpoint(epoch, model, optimizer, val_acc, is_best):
     state = {'model': model,
              'optimizer': optimizer}
-    filename = 'checkpoint_{}_{}.tar'.format(epoch, val_acc)
+    filename = 'checkpoint_{}_{.3f}.tar'.format(epoch, val_acc)
     torch.save(state, filename)
     # If this checkpoint is the best so far, store a copy so it doesn't get overwritten by a worse checkpoint
     if is_best:
