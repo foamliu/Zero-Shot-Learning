6fbcf8a5a0a00a205314dda1143c029f5894d221
Yang Liu <foamliu@yeah.net>
2018-09-30 17:07:06 +0800

    update

--- demo.py
+++ demo.py
@@ -19,8 +19,7 @@ def main():
     model = checkpoint['model']
     # model = model.to(device)
     model = model.cuda()
-    model.eval()
-    print('model.device: ' + str(model.device))
+    # model.eval()
 
     files = [os.path.join(zsl_a_animals_test_folder, file) for file in os.listdir(zsl_a_animals_test_folder) if
              file.lower().endswith('.jpg')]
